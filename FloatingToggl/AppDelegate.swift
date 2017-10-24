1c8d7260476566f0c728ee6d516e9b101c698ee8
Zhigang Fang <zhigang1992@gmail.com>
2017-10-24 11:51:20 +0800

    Initial Commit

---
+++ FloatingToggl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FloatingToggl
+//
+//  Created by Zhigang Fang on 10/24/17.
+//  Copyright © 2017 matrix. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FloatingToggl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FloatingToggl
+//
+//  Created by Zhigang Fang on 10/24/17.
+//  Copyright © 2017 matrix. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
